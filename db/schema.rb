ef1473bebf8a80631016fc1b858427b62de7c319
Yusuke-Koiwa <smallrock0109@gmail.com>
2020-07-02 15:53:24 +0900

    Pictureモデルの作成

--- db/schema.rb
+++ db/schema.rb
@@ -10,12 +10,24 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2020_07_02_002614) do
+ActiveRecord::Schema.define(version: 2020_07_02_063507) do
 
   create_table "categories", options: "ENGINE=InnoDB DEFAULT CHARSET=utf8", force: :cascade do |t|
     t.string "name"
     t.string "ancestry"
     t.index ["ancestry"], name: "index_categories_on_ancestry"
+  end
+
+  create_table "pictures", options: "ENGINE=InnoDB DEFAULT CHARSET=utf8", force: :cascade do |t|
+    t.string "titile", null: false
+    t.string "image", null: false
+    t.bigint "user_id", null: false
+    t.bigint "category_id", null: false
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+    t.index ["category_id"], name: "index_pictures_on_category_id"
+    t.index ["titile"], name: "index_pictures_on_titile"
+    t.index ["user_id"], name: "index_pictures_on_user_id"
   end
 
   create_table "users", options: "ENGINE=InnoDB DEFAULT CHARSET=utf8", force: :cascade do |t|
@@ -33,4 +45,6 @@ ActiveRecord::Schema.define(version: 2020_07_02_002614) do
     t.index ["reset_password_token"], name: "index_users_on_reset_password_token", unique: true
   end
 
+  add_foreign_key "pictures", "categories"
+  add_foreign_key "pictures", "users"
 end
