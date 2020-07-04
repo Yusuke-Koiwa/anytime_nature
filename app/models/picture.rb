5d5ebf6165f7697d87b0900802c3414f1897be3b
Yusuke-Koiwa <smallrock0109@gmail.com>
2020-07-04 14:48:48 +0900

    前後の詳細ページへのリンク作成

--- app/models/picture.rb
+++ app/models/picture.rb
@@ -5,4 +5,12 @@ class Picture < ApplicationRecord
   mount_uploader :image, ImageUploader
 
   validates :title, :image, presence: true
+
+  def previous
+    Picture.where("id > ?", self.id).order("id ASC").first
+  end
+
+  def next
+    Picture.where("id < ?", self.id).order("id DESC").first
+  end
 end
