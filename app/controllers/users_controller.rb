fe8a24f495a21dbd37ebdb5cf2efad2b5fe92da5
Yusuke-Koiwa <smallrock0109@gmail.com>
2020-07-05 09:34:08 +0900

    ユーザーの投稿一覧ページ作成

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -2,6 +2,7 @@ class UsersController < ApplicationController
   before_action :set_user
 
   def show
+    @pictures = @user.pictures.order("created_at DESC").page(params[:page]).per(20)
   end
 
   private
