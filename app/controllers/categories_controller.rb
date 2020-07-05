22eb181b638da7b01788a505c7da6d28189b789b
Yusuke-Koiwa <smallrock0109@gmail.com>
2020-07-05 18:14:09 +0900

    他カテゴリーへのリンク作成

--- app/controllers/categories_controller.rb
+++ app/controllers/categories_controller.rb
@@ -1,4 +1,5 @@
 class CategoriesController < ApplicationController
+  before_action :set_category, only: :show
 
   def index
     @parents = Category.where(ancestry: nil)
@@ -6,12 +7,22 @@ class CategoriesController < ApplicationController
   end
 
   def show
-    @category = Category.find(params[:id])
     @pictures = @category.set_pictures.order("created_at DESC").page(params[:page]).per(20)
   end
-
+  
   def children
     @children = Category.find(params[:parentCategory]).children
+  end
+  
+  private
+  
+  def set_category
+    @category = Category.find(params[:id])
+    if @category.root?
+      @category_links = @category.children
+    else
+      @category_links = @category.siblings
+    end
   end
 
 end
