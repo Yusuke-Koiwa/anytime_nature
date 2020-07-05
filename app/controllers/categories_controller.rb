c72663d670637f17be03e33c0f675625021ad038
Yusuke-Koiwa <smallrock0109@gmail.com>
2020-07-05 17:35:27 +0900

    カテゴリー一覧ページ作成

--- app/controllers/categories_controller.rb
+++ app/controllers/categories_controller.rb
@@ -1,5 +1,13 @@
 class CategoriesController < ApplicationController
 
+  def index
+    @parents = Category.where(ancestry: nil)
+    @images = ["forest.jpg", "flower.jpg", "mountain.jpg", "ground.jpg", "sea.jpg", "river.jpg", "sky.jpg", "snow.jpg"]
+  end
+
+  def show
+  end
+
   def children
     @children = Category.find(params[:parentCategory]).children
   end
