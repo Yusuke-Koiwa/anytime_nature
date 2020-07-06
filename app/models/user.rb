b85c9ed426967880b41f823001b32d8520507a8b
Yusuke-Koiwa <smallrock0109@gmail.com>
2020-07-06 17:05:24 +0900

    お気に入り追加/削除メソッド作成

--- app/models/user.rb
+++ app/models/user.rb
@@ -4,9 +4,23 @@ class User < ApplicationRecord
 
   has_many :pictures, dependent: :destroy
   has_many :favorites, dependent: :destroy
+  has_many :favorite_pictures, through: :favorites, source: :picture
 
   mount_uploader :image, IconsUploader
   
   validates :name, presence: true
   validates :email, presence: true, uniqueness: true
+
+  def already_favorite?(picture)
+    favorite_pictures.include?(picture)
+  end
+
+  def create_favorite(picture)
+    favorites.find_or_create_by(picture_id: picture.id)
+  end
+
+  def delete_favorite(picture)
+    favorite = favorites.find_by(picture_id: picture.id)
+    favorite&.destroy
+  end
 end
