1b1baa367ffeaab4cd2b7ca98e0638726822dd65
Yusuke-Koiwa <smallrock0109@gmail.com>
2020-07-06 17:04:22 +0900

    アクション定義

--- app/controllers/favorites_controller.rb
+++ app/controllers/favorites_controller.rb
@@ -1,2 +1,23 @@
 class FavoritesController < ApplicationController
+  before_action :move_to_login, unless: :user_signed_in?
+  before_action :set_picture
+
+  def create
+    @favorite = current_user.create_favorite(@picture)
+  end
+
+  def destroy
+    @favorite = current_user.delete_favorite(@picture)
+  end
+
+  private
+
+  def set_picture
+    @picture = Picture.find(params[:picture_id])
+  end
+
+  def move_to_login
+    flash[:alert] = "ログインが必要です"
+    redirect_to new_user_session_path
+  end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -2,7 +2,9 @@ Rails.application.routes.draw do
   devise_for :users
   root to: "pictures#index"
   resources :users, only: [:show, :edit, :update]
-  resources :pictures
+  resources :pictures do
+    resources :favorites, only: [:create, :destroy]
+  end
   resources :categories, only: [:index, :show] do
     collection do
       get "children"
