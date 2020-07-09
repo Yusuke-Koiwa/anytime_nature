7edc635cfaca1addfea340f59ad3a74e17e1b3a4
Yusuke-Koiwa <smallrock0109@gmail.com>
2020-07-09 17:04:40 +0900

    ログイン後に元のページに遷移

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,9 +1,21 @@
 class ApplicationController < ActionController::Base
   before_action :configure_permitted_parameters, if: :devise_controller?
+  before_action :store_current_location, unless: :devise_controller?
+
+  def after_sign_in_path_for(resource)
+    stored_location_for(resource) || root_path
+  end
 
   protected
 
   def configure_permitted_parameters
     devise_parameter_sanitizer.permit(:sign_up, keys: %i[name image])
   end
+
+  private
+
+  def store_current_location
+    store_location_for(:user, request.url)
+  end
+
 end
