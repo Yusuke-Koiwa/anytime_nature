4703cd7a4fa2e314a2823da4fc2d510e4111127a
Yusuke-Koiwa <smallrock0109@gmail.com>
2020-07-11 08:44:08 +0900

    公開鍵の設定修正

--- config/deploy.rb
+++ config/deploy.rb
@@ -9,10 +9,6 @@ set :linked_dirs, fetch(:linked_dirs, []).push('log', 'tmp/pids', 'tmp/cache', '
 set :rbenv_type, :user
 set :rbenv_ruby, '2.5.1'
 
-# どの公開鍵を利用してデプロイするか
-set :ssh_options, auth_methods: ['publickey'],
-                  keys: ['~/.ssh/anytime-nature_key_rsa'] 
-
 # プロセス番号を記載したファイルの場所
 set :unicorn_pid, -> { "#{shared_path}/tmp/pids/unicorn.pid" }
 
