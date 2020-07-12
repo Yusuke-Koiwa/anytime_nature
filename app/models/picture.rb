44327a863ac9d74efb233a9f47503ca32a404a2c
Yusuke-Koiwa <smallrock0109@gmail.com>
2020-07-12 09:10:04 +0900

    タグ更新メソッドの作成

--- app/models/picture.rb
+++ app/models/picture.rb
@@ -8,4 +8,19 @@ class Picture < ApplicationRecord
   mount_uploader :image, ImageUploader
 
   validates :title, :image, presence: true
+
+  def save_tags(tags)
+    current_tags = self.tags.pluck(:name) unless self.tags.nil?
+    old_tags = current_tags - tags
+    new_tags = tags - current_tags
+
+    old_tags.each do |old_name|
+      self.tags.delete Tag.find_by(name: old_name)
+    end
+
+    new_tags.each do |new_name|
+      picture_tag = Tag.find_or_create_by(name: new_name)
+      self.tags << picture_tag
+    end
+  end
 end
