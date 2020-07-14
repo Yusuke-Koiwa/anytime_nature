b3a895d1d244ae7e161c6d6ebf14eac89bf8977e
Yusuke-Koiwa <smallrock0109@gmail.com>
2020-07-14 16:30:31 +0900

    コメント送信時に通知作成

--- app/models/picture.rb
+++ app/models/picture.rb
@@ -39,4 +39,23 @@ class Picture < ApplicationRecord
     notification.save if notification.valid?
   end
 
+  def create_notification_comment(current_user, comment_id)
+    temp_ids = Comment.select(:user_id).where(picture_id: id).where.not(user_id: current_user.id).distinct
+    temp_ids.each do |temp_id|
+      save_notification_comment(current_user, comment_id, temp_id['user_id'])
+    end
+    save_notification_comment(current_user, comment_id, user_id) if temp_ids.blank?
+  end
+
+  def save_notification_comment(current_user, comment_id, visited_id)
+    notification = current_user.active_notifications.new(
+      picture_id: id,
+      comment_id: comment_id,
+      visited_id: visited_id,
+      action: 'comment'
+    )
+    notification.checked = true if notification.visitor_id == notification.visited_id
+    notification.save if notification.valid?
+  end
+
 end
