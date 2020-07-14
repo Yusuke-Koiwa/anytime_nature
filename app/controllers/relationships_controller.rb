6019bc642c09b4f3d1ce033e68982965b2b3ef38
Yusuke-Koiwa <smallrock0109@gmail.com>
2020-07-14 16:14:18 +0900

    フォロー/フォロー解除時に通知を作成/削除

--- app/controllers/relationships_controller.rb
+++ app/controllers/relationships_controller.rb
@@ -4,10 +4,12 @@ class RelationshipsController < ApplicationController
 
   def create
     current_user.follow(@user)
+    @user.create_notification_follow(current_user)
   end
 
   def destroy
     current_user.unfollow(@user)
+    Notification.find_by(visitor_id: current_user.id, visited_id: @user.id, action: "follow").destroy
   end
 
   private
