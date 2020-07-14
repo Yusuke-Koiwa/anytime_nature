8779ea710f6b5b3c69b5cd7fcc84e1dfc1e2e7f1
Yusuke-Koiwa <smallrock0109@gmail.com>
2020-07-14 11:17:41 +0900

    create アクション作成

--- app/controllers/comments_controller.rb
+++ app/controllers/comments_controller.rb
@@ -1,2 +1,22 @@
 class CommentsController < ApplicationController
+  before_action :move_to_login, unless: :user_signed_in?
+
+  def create
+    @picture = Picture.find(params[:picture_id])
+    @comment = @picture.comments.build(comment_params)
+    @comment.save
+    @comments = @comment.picture.comments
+  end
+
+  private
+  
+  def move_to_login
+    flash[:alert] = "ログインが必要です"
+    redirect_to new_user_session_path
+  end
+
+  def comment_params
+    params.require(:comment).permit(:text).merge(user_id: current_user.id)
+  end
+
 end
