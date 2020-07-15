dd072f9933869dabc922b3082430a96d09a95448
Yusuke-Koiwa <smallrock0109@gmail.com>
2020-07-15 19:03:28 +0900

    レイアウト修正

--- app/assets/javascripts/slideshow.js
+++ app/assets/javascripts/slideshow.js
@@ -1,29 +1,31 @@
 $(function() {
 
-  $('#slideshow-contents').slick({
-      dots: true,
-      autoplay: false,
-      autoplaySpeed: 3000,
-      infinite: true,
-      speed: 500,
-      fade: true,
-      cssEase: 'linear'
-  });
-
-  $('.slick-dots li').on('mouseover', function() {
-    $('#slideshow-contents').slick('goTo', $(this).index());
-  });
-
-  $('#play-btn').on('click', function(){
-    $('#play-btn').hide();
-    $('#stop-btn').show();
-    $('#slideshow-contents').slick('slickPlay');
-  });
-
-  $('#stop-btn').on('click', function(){
-    $('#stop-btn').hide();
-    $('#play-btn').show();
-    $('#slideshow-contents').slick('slickPause');
-  });
+  if ($('#slideshow-contents').length) {
+    $('#slideshow-contents').slick({
+        dots: true,
+        autoplay: false,
+        autoplaySpeed: 3000,
+        infinite: true,
+        speed: 500,
+        fade: true,
+        cssEase: 'linear'
+    });
+  
+    $('.slick-dots li').on('mouseover', function() {
+      $('#slideshow-contents').slick('goTo', $(this).index());
+    });
+  
+    $('#play-btn').on('click', function(){
+      $('#play-btn').hide();
+      $('#stop-btn').show();
+      $('#slideshow-contents').slick('slickPlay');
+    });
+  
+    $('#stop-btn').on('click', function(){
+      $('#stop-btn').hide();
+      $('#play-btn').show();
+      $('#slideshow-contents').slick('slickPause');
+    });
+  }
 
 });
