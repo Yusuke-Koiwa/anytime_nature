371b7d62164578f4bae8e5291c6e68d5dfa23c17
Yusuke-Koiwa <smallrock0109@gmail.com>
2020-07-19 12:05:37 +0900

    Update production.rb

--- config/deploy/production.rb
+++ config/deploy/production.rb
@@ -1,5 +1,4 @@
 server '54.178.118.236', user: 'koiwa', roles: %w{app db web}
 
 # どの公開鍵を利用してデプロイするか
-set :ssh_options, auth_methods: ['publickey'],
-                  keys: ['~/.ssh/anytime-nature.pem']
+set :ssh_options, keys: '~/.ssh/anytime-nature_key_rsa' 
