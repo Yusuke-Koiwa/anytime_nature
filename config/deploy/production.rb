b60d3253563ead41dc48abb11854d226b179b865
Yusuke-Koiwa <smallrock0109@gmail.com>
2020-07-19 12:16:14 +0900

    デプロイの設定

--- config/deploy/production.rb
+++ config/deploy/production.rb
@@ -1,4 +1,5 @@
 server '54.178.118.236', user: 'koiwa', roles: %w{app db web}
 
 # どの公開鍵を利用してデプロイするか
-set :ssh_options, keys: '~/.ssh/anytime-nature_key_rsa' 
+set :ssh_options, auth_methods: ['publickey'],
+                  keys: ['~/.ssh/anytime-nature_key_rsa']
