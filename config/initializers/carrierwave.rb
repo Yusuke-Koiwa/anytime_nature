c34bb0c9f1346c7a3e812e67238e880581417834
Yusuke-Koiwa <smallrock0109@gmail.com>
2020-11-01 16:17:07 +0900

    Update carrierwave.rb

--- config/initializers/carrierwave.rb
+++ config/initializers/carrierwave.rb
@@ -8,6 +8,7 @@ CarrierWave.configure do |config|
   else
     config.storage = :fog
     config.fog_provider = 'fog/aws'
+    config.fog_directory = 'anytime-nature-bucket'
     config.fog_credentials = {
       provider: 'AWS',
       aws_access_key_id: Rails.application.credentials[:aws][:access_key_id],
@@ -15,7 +16,6 @@ CarrierWave.configure do |config|
       region: 'ap-northeast-1'
     }
 
-    config.fog_directory = 'anytime-nature-bucket'
-    config.asset_host = 'https://s3-ap-northeast-1.amazonaws.com/anytime-nature-bucket'
+    # config.asset_host = 'https://s3-ap-northeast-1.amazonaws.com/anytime-nature-bucket'
   end
 end
