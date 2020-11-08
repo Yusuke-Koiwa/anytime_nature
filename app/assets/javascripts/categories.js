f5ed8234cfc1b2f8a70f3815083a5508bee8ee1e
Yusuke-Koiwa <smallrock0109@gmail.com>
2020-11-08 17:40:45 +0900

    写真投稿ページ UI改善

--- app/assets/javascripts/categories.js
+++ app/assets/javascripts/categories.js
@@ -7,39 +7,32 @@ $(function(){
     return html
   }
 
-  function appendChildrenSelection(insertHtml){
-    let childrenSelectHtml = "";
-    childrenSelectHtml = `<select class="select-default" id="children-categories" name="picture[category_id]">
-                            <option value="">選択して下さい</option>
-                            ${insertHtml}
-                          </select>`;
-    $(".category-input").append(childrenSelectHtml);
-  }
-
   $(document).on("change", "#category-select", function(){
     let parentCategory = $("#category-select").val();
     if (parentCategory == "") {
-      $("#children-categories").remove();
-    } else {
-      $.ajax({
-        url: "/categories/children",
-        type: "GET",
-        data: {parentCategory: parentCategory},
-        dataType: "json"
-      })
-      .done(function(children){
-        let insertHtml = "";
-        children.forEach(function(child){
-          insertHtml += appendOption(child);
-        });
-        if ($("#children-categories").length) {
-          $("#children-categories").remove();
-        }
-        appendChildrenSelection(insertHtml);
-      })
-      .fail(function(){
-        alert("カテゴリーを取得出来ませんでした")
-      })
+      $("#children-categories").empty();
+      let insertHtml = '<option value="">選択して下さい</option>';
+      $('#children-categories').append(insertHtml);
+
+      return;
     }
+
+    $.ajax({
+      url: "/categories/children",
+      type: "GET",
+      data: {parentCategory: parentCategory},
+      dataType: "json"
+    })
+    .done(function(children){
+      $('#children-categories').empty();
+      let insertHtml = '<option value="">選択して下さい</option>';
+      children.forEach(function(child){
+        insertHtml += appendOption(child);
+      });
+      $('#children-categories').append(insertHtml);
+    })
+    .fail(function(){
+      alert("カテゴリーを取得出来ませんでした")
+    })
   });
 });
