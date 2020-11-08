e6eeae0635617b95cc086aa94384a89b9f7997ee
Yusuke-Koiwa <smallrock0109@gmail.com>
2020-11-08 18:38:46 +0900

    写真投稿　エラー文の表示

--- app/assets/javascripts/post-form.js
+++ app/assets/javascripts/post-form.js
@@ -1,10 +1,46 @@
 $(function(){
+  let preventSubmit = true;
 
   function appendOption(category){
     let html = `<option value="${category.id}">
                   ${category.name}
                 </option>`;
     return html
+  }
+
+  function checkUpload(errorCount) {
+    if ($('.prev-content').length >= 1) {
+
+      return errorCount;
+    }
+    let el = $('.image_label').parent('.field__input');
+    let message = "画像をアップロードして下さい";
+    appendErrorMessage(el, message);
+    return errorCount + 1;
+  }
+
+  function checkCategory(errorCount) {
+    if ($('#category-select').val() == "") {
+      let el = $('#main-category');
+      let message = "メインカテゴリーを選択して下さい";
+      appendErrorMessage(el, message);
+
+      return errorCount + 1;
+    }
+    if ($('#children-categories').val() == "") {
+      let el = $('#sub-category');
+      let message = "サブカテゴリーを選択して下さい";
+      appendErrorMessage(el, message);
+
+      return errorCount + 1;
+    }
+
+    return errorCount;
+  }
+
+  function appendErrorMessage(el, message){
+    let errorMessage = `<div class="error-message">${message}</div>`
+    el.append(errorMessage);
   }
 
   $(document).on("change", "#category-select", function(){
@@ -16,7 +52,6 @@ $(function(){
 
       return;
     }
-
     $.ajax({
       url: "/categories/children",
       type: "GET",
@@ -35,4 +70,22 @@ $(function(){
       alert("カテゴリーを取得出来ませんでした")
     })
   });
+
+  $('#pic-submit-btn').on('click', function(e){
+    if (preventSubmit === true) {
+      e.preventDefault();
+      $('.error-message').each(function() {
+        $(this).remove();
+      });
+      let errorCount = 0;
+      errorCount = checkUpload(errorCount);
+      errorCount = checkCategory(errorCount);
+      if (errorCount !== 0) {
+
+        return;
+      }
+      preventSubmit = false;
+      $('#pic-submit-btn').trigger('click');
+    }
+  })
 });
