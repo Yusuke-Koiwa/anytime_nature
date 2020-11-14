df74ac02d34bef58a3317e70d3ebbba5c2cfa033
Yusuke-Koiwa <smallrock0109@gmail.com>
2020-11-14 19:36:41 +0900

    写真ホバー時のアニメーション改善

--- app/assets/javascripts/picture-info.js
+++ app/assets/javascripts/picture-info.js
@@ -3,7 +3,9 @@ $(function(){
 
   pictureClass.hover(function(){
     $(this).children('.picture-info').addClass('visible');
+    $(this).find('.picture-image').addClass('zoom-in');
   }, function(){
     $(this).children('.picture-info').removeClass('visible');
+    $(this).find('.picture-image').removeClass('zoom-in');
   });
 });
